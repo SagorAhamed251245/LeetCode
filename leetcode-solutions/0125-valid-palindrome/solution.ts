570a135c27fc62b4b28c095843bb0dd2c29084ee
Sagor Ahamed <104268039+SagorAhamed251245@users.noreply.github.com>
2026-07-17 05:21:12 +0000

    Sync LeetCode submission Runtime - 12 ms (11.04%), Memory - 62.9 MB (11.76%)

---
+++ leetcode-solutions/0125-valid-palindrome/solution.ts
@@ -0,0 +1,30 @@
+function isPalindrome(s: string): boolean {
+    let i = 0;
+    let j = s.length - 1;
+    function nonAlpha(str: string) {
+        return str.replace(/[^a-zA-Z0-9]/g, "");
+    }
+
+    while (i < j) {
+        const left = s[i]!;
+        const right = s[j]!;
+        if (nonAlpha(left) === "") {
+            i++;
+            continue;
+        }
+
+        if (nonAlpha(right) === "") {
+            j--;
+            continue;
+        }
+
+        if (nonAlpha(left).toLowerCase() !== nonAlpha(right).toLowerCase()) {
+            return false;
+        }
+
+        i++;
+        j--;
+    }
+
+    return true;
+};
