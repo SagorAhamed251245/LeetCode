b02161cc10c59d389c423406a87cebb7a1a0ef31
Sagor Ahamed <104268039+SagorAhamed251245@users.noreply.github.com>
2026-07-17 03:52:33 +0000

    Sync LeetCode submission Runtime - 35 ms (83.43%), Memory - 80.9 MB (75.94%)

---
+++ leetcode-solutions/0128-longest-consecutive-sequence/solution.ts
@@ -0,0 +1,19 @@
+function longestConsecutive(nums: number[]): number {
+   if (nums.length === 0 || nums === null) {
+    return 0;
+  }
+  const set = new Set(nums);
+  let longest = 0;
+  for (const num of set) {
+    if (!set.has(num - 1)) {
+      let currentNum = num;
+      let currentStreak = 1;
+      while (set.has(currentNum + 1)) {
+        currentNum++;
+        currentStreak++;
+      }
+      longest = Math.max(longest, currentStreak);
+    }
+  }
+  return longest;
+};
