20929b3801b1327852b598fa1acb8fd74660d054
Sagor Ahamed <104268039+SagorAhamed251245@users.noreply.github.com>
2026-07-22 04:54:54 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 54.5 MB (80.19%)

---
+++ leetcode-solutions/0027-remove-element/solution.ts
@@ -0,0 +1,12 @@
+function removeElement(nums: number[], val: number): number {
+    let k = 0;
+    for (const num of nums) {
+        if (num !== val) {
+            nums[k] = num;
+
+            k++;
+        }
+    }
+
+    return k;
+};
